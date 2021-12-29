07a903caf23ee9145c55a52df53e10b8f0c46274
Tekiter <tekiter8@gmail.com>
2021-12-29 18:22:35 +0900

    feat: add exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+export { isArray } from "./validate/array";
+export { isBoolean, isNull } from "./validate/misc";
 export { isNumber } from "./validate/number";
+export { isObject } from "./validate/object";
+export { isOr } from "./validate/or";
 export { isString } from "./validate/string";
